62eb54c9005802244f41c7edf3c9b026d96a734c
Petr <povar_cr@mail.ru>
2022-02-14 13:22:54 +0300

    chenge Task_28

--- Task_28/Program.cs
+++ Task_28/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+while (num > 0)
+{
+    sum = sum + num % 10;
+    num = num / 10;
+}
+Console.WriteLine($"Сумма цифр числа: {sum}");
