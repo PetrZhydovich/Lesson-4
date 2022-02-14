e9dbf1a77eb847a2bbe94e15a52ba7eac3ba3e77
Petr <povar_cr@mail.ru>
2022-02-14 17:06:18 +0300

    add Task_32

--- Task_28/Program.cs
+++ Task_28/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Введите число: ");
 int num = int.Parse(Console.ReadLine());
 int sum = 0;
-while (num > 0)
+while (num > 0) 
 {
     sum = sum + num % 10;
     num = num / 10;
---
+++ Task_32/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+void FillArray(int[] collection) 
+{
+    int lenght = collection.Length; 
+    int index = 0;
+    while (index<lenght)
+    {
+        collection[index] = new Random().Next(0,2);  
+        index++; 
+    } 
+}
+
+void PrintArray(int[] col) //col=collection Эта функция выводит массив на экран
+{
+    int count = col.Length;
+    int position = 0; //position=index
+    while(position<count)
+    {
+        Console.WriteLine(col[position]);
+        position++;  
+    } 
+}
+
+int[] array = new int[8]; //"создал новый массив в котором будет 8 элементов"
+
+FillArray(array); //заполнил массив
+PrintArray(array); //распечатал массив
+
+   
