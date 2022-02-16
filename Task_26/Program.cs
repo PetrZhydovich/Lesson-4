e382364c9941cca693c9767cb1a8cacee28a0244
Petr <povar_cr@mail.ru>
2022-02-16 20:36:18 +0300

    add Task_26

---
+++ Task_26/Program.cs
@@ -0,0 +1,9 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+double GetRandomNumber()
+{
+    double number = new Random().Next(-20, 20);
+    return number;   
+}
+double a = GetRandomNumber();
+double b = GetRandomNumber();
+Console.WriteLine($"Сгенерироавнное число: {a} \n Сгенерированная степени числаL: {b}");
