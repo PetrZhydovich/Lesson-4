ed7749922e8d540d420603c23da3b56fe7762d6e
Petr <povar_cr@mail.ru>
2022-02-17 12:36:29 +0300

    Change task_30

--- Task_30/Program.cs
+++ Task_30/Program.cs
@@ -1,2 +1,38 @@
 ﻿//  Показать кубы чисел, заканчивающихся на четную цифру
 
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = new Random().Next(1, 100);
+        index++;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine($"{arr[position]} ");
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
+double result = 0;
+int index = 0;
+int power = 3;
+while (array[index] % 2 == 0)
+{
+    result = Math.Pow(array[index], power);
+    Console.WriteLine($"{array[index]} ^ {power} = {result}");
+    index++;
+}
